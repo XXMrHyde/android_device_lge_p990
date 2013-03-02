9dffdc72e082afbd3bb227b29f9ad8f443bc1a3a
XXMrHyde <xxmrhyde@AwesomeMashine.(none)>
2013-03-02 23:43:39 +0100

    Added changes for Dark Jelly 2X Edition

--- p990.mk
+++ p990.mk
@@ -1,3 +1,5 @@
+DEVICE_PACKAGE_OVERLAYS += device/lge/p990/overlay
+
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_eu.mk)
 
@@ -24,6 +26,23 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     lgcpversion
+
+# Dark Jelly 2X Edition additions
+
+# Copy over the Dark Jelly 2X Edition changelog to the device
+PRODUCT_COPY_FILES += \
+    device/lge/p990/CHANGELOG-DJE.txt:system/etc/CHANGELOG-DJE.txt
+
+# Dark Jelly 2X Edition Property Overrides
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.xxmrhyde.version=Dark-Jelly-2X-Edition-(Build-1) \
+    updateme.name=Dark-Jelly-2X-Edition \
+    updateme.version=1.0.0 \
+	pdateme.urlcheck=http://roms.custom-android.de/files/Roms/p990/CM10/update_me_ckeck.xml \
+    updateme.reboottype=2 \
+    updateme.disableinstalledapps=1 \
+    updateme.disablescripts=1
 
 PRODUCT_NAME := full_p990
 PRODUCT_DEVICE := p990
